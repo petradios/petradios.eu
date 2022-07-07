119b37367b4825363de034bf1402370dfe98ac87
petradios <73046351+petradios@users.noreply.github.com>
2022-07-08 02:19:12 +0300

    this is not working code will fix later

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,38 @@
+const theme = localStorage.getItem('theme');
+
 function themeSwitcher() {
     const body = document.body;
-    const bttn = document.getElementById("theme-toggle");
+    
+    alert(theme);
+    //if (theme){
+      //  body.classList.add(theme);
+    //}
+
+
 
     if(body.className == "dark-theme"){
-        body.classList.replace('dark-theme', 'light-theme');
-        bttn.innerHTML = "dark theme";
-        return;
-    }
-    if(body.className == "light-theme"){
-        body.classList.replace('light-theme', 'dark-theme');
+        body.classList.replace("dark-theme", "light-theme");
+        theme.setItem(theme, dark-theme);
         bttn.innerHTML = "light theme";
         return;
     }
+    if(body.className == "light-theme"){
+        body.classList.replace("light-theme", "dark-theme");
+        localStorage.setItem(theme, light-theme);
+        bttn.innerHTML = "dark theme";
+        return;
+    }
+}
+
+function themeSwitcher2(){
+    const body = document.body;
+    let check = document.getElementById("check");
+
+    if (check.checked == true){
+        body.classList.replace('dark-theme', 'light-theme');
+    }
+    else{
+        body.classList.replace('light-theme', 'dark-theme');
+    }
+    
 }
